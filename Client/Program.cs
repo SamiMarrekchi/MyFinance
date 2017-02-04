108b9142f1b17df1fc993bdd5e16bd41d402f605
Sami Marrekchi <Sami Marrekchi@DESKTOP-P94TF26>
2017-02-04 09:35:36 +0100

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,29 @@
+﻿using Data;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Context ctxt = new Context();
+            Category cat = new Category
+            {
+                Name = "Category1",
+
+            };
+            ctxt.Categorys.Add(cat);
+            ctxt.SaveChanges();
+            System.Console.WriteLine("sauvgarde reussie");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Data/Context.cs
@@ -0,0 +1,18 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Context : DbContext
+    {
+        public Context():base("MyFinanceDB"){}
+        public DbSet<Provider> Providers { get; set; }
+        public DbSet<Category> Categorys { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
